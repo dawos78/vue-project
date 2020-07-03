8e924ef0f0756397c17452297fad57591bd2f6ea
dawos71042 <minimojo78@hotmail.com>
2020-07-03 21:59:28 +0700

    initial commit

---
+++ src/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="test">
+    <router-link to="page1">page1</router-link>
+    <router-link to="page2">page2</router-link>
+    <router-link to="page3">page3</router-link>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+name : 'test',
+}
+</script>
+
+<style>
+
+</style>
